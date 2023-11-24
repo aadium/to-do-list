4cc1e58e41cff41df6c52bf9d817188dfedd7c8f
Aadi Umrani <47914987+aadium@users.noreply.github.com>
2023-11-24 13:23:37 -0500

    can add tasks

--- backend/server.js
+++ backend/server.js
@@ -8,17 +8,37 @@ const PORT = 3002;
 app.use(cors());
 app.use(express.json())
 
-// Route to get all posts 
 app.get("/api/get", (req, res) => {
   db.query("SELECT * FROM todo_tasks", (err, result) => {
     if (err) {
       console.log(err)
     }
     res.send(result)
-    console.log(result);
   });
 });
 
-app.listen(PORT, ()=>{
+app.post('/api/post', (req, res) => {
+  const username = req.body.userName;
+  const title = req.body.title;
+  const completed = req.body.completed;
+
+  db.query("INSERT INTO todo_tasks VALUES (?,?,?,?)", [username, title, completed, Date.now()], (err, result) => {
+    if (err) {
+      console.log(err)
+    }
+  });
+})
+
+app.delete('/api/delete/:id', (req, res) => {
+  const id = req.params.id;
+
+  db.query("DELETE FROM todo_tasks WHERE id= ?", id, (err, result) => {
+    if (err) {
+      console.log(err)
+    }
+  })
+})
+
+app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`)
 })
--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import Checkbox from '@mui/material/Checkbox';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+function AddTask() {
+  const [taskName, setTaskName] = useState('');
+  function handleAdd() {
+    fetch('http://localhost:3002/api/post', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: 'test', title: taskName, completed: false }),
+    }).catch(error => console.error('Error adding task:', error));
+  }
+  return (
+    <center>
+      <div className='content'>
+        <p>Add Task</p>
+        <input type='text' id='taskName' value={taskName} onChange={(e) => setTaskName(e.target.value)} />
+      </div>
+      <div>
+        <button className='secondary-button' onClick={() => handleAdd()}>
+          Add Task
+        </button>
+      </div>
+    </center>
+  )
+}
+
 function TaskRow({ task }) {
   const [completed, setCompletionStatus] = useState(Boolean(task.completed));
   const handleCompletionStatusChange = () => {
@@ -18,40 +44,14 @@ function TaskRow({ task }) {
 function TasksTable() {
   let rows = [];
   const [taskList, setTaskList] = useState([]);
-  const [taskName, setTaskName] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:3002/api/get')
       .then(response => response.json())
       .then(data => setTaskList(data))
       .catch(error => console.error('Error fetching tasks:', error));
-    
-    console.log(taskList);
   }, []);
 
-  function handleAdd() {
-    fetch('/api/tasks', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ taskName, completed: false }),
-    })
-      .then(response => {
-        if (response.ok) {
-          console.log('Task added successfully');
-          // Optionally, you can fetch the updated task list after adding a task
-          // This depends on your use case
-          // fetch('/api/tasks')
-          //   .then(response => response.json())
-          //   .then(data => setTaskList(data))
-          //   .catch(error => console.error('Error fetching tasks:', error));
-        } else {
-          console.error('Error adding task:', response.statusText);
-        }
-      })
-      .catch(error => console.error('Error adding task:', error));
-  }
   taskList.forEach(task => {
     rows.push(<TaskRow task={task} key={task.taskName}></TaskRow>);
   });
@@ -66,17 +66,6 @@ function TasksTable() {
         </thead>
         <tbody>{rows}</tbody>
       </table>
-      <center>
-        <div className='content'>
-          <p>Add Task</p>
-          <input type='text' id='taskName' value={taskName} onChange={(e) => setTaskName(e.target.value)} />
-        </div>
-        <div>
-          <button className='secondary-button' onClick={() => handleAdd()}>
-            Add Task
-          </button>
-        </div>
-      </center>
     </div>
   );
 }
@@ -85,6 +74,7 @@ function App() {
   return (
     <div>
       <TasksTable/>
+      <AddTask/>
     </div>
   )
 }
